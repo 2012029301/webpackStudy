2101a56b73a40cd21c9c6d01b466adf76088cdf4
lijun <2012029301@qq.com>
2022-07-19 23:01:28 +0800

    webpack 学习

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 import { sum } from "./js/sum";
+import "./css/index.css";
+import "./less/index.less";
 console.log(sum(1, 2, 3));
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,32 @@ module.exports = {
   module: {
     rules: [
       // loader的配置
+      {
+        test: /\.css$/i, //检查以.css 结尾的文件
+        use: [
+          "style-loader", //将js中css通过创建style标签添加到html文件中生效
+          "css-loader", //将css资源编译成common.js的模块js中
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          //use 可以使用多个loader loader 只能使用一个
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 10kb 小于10k 转成bas64 不过体积会大一点
+          },
+        },
+      },
     ],
   },
   //   插件
